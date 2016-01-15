84b3a9f4cb66459b022446180893f2ed210e4589
Suraj B. Malode <suraj0x92@gmail.com>
2016-01-15 11:38:43 +0000

    Added localStorage support for persistent data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,13 @@
  * Controller of the todoApp
  */
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var toDosInStore = localStorageService.get('todos');
+    
+    $scope.todos = toDosInStore || [];
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
     
     $scope.addToDo = function() {
       $scope.todos.push($scope.todo);
@@ -18,5 +23,5 @@ angular.module('todoApp')
     
     $scope.removeToDo = function(index) {
       $scope.todos.splice(index,1);
-    }
+    };
   });
