5964ce9d6c34459ecab80199eb83b1852fa295f5
Suraj B. Malode <suraj0x92@gmail.com>
2016-01-15 10:05:31 +0000

    Add basic todo container

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,11 @@
  * Controller of the todoApp
  */
 angular.module('todoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'Generate Scaffold',
+      'Modify App',
+      'Commit',
+      'Push'
     ];
   });
