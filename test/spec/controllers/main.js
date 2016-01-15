60d862624dfc168c50ceec147eaec601bd15d61c
Suraj B. Malode <suraj0x92@gmail.com>
2016-01-15 11:26:29 +0000

    Added more tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -20,4 +20,17 @@ describe('Controller: MainCtrl', function () {
   it('should have no todo items initially', function () {
     expect(scope.todos.length).toBe(0);
   });
+  
+  it('should add todos to the list', function() {
+    scope.todo = 'Test 0';
+    scope.addToDo();
+    expect(scope.todos.length).toBe(1);
+  });
+  
+  it('should add then remove a todo from the list', function () {
+    scope.todo = 'Test 0';
+    scope.addToDo();
+    scope.removeToDo(0);
+    expect(scope.todos.length).toBe(0);
+  });
 });
